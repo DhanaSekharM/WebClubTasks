c45f8c2a714e692d1ee45ac6ca9e61c0295272ef
Dhanasekhar <sekhardhana529@gmail.com>
2019-08-20 00:31:30 +0530

    Add basic text recognition

--- app/src/main/java/com/example/mobilevision/HomePageActivity.java
+++ app/src/main/java/com/example/mobilevision/HomePageActivity.java
@@ -2,14 +2,27 @@ package com.example.mobilevision;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class HomePageActivity extends AppCompatActivity {
 
+    FloatingActionButton openCameraFab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        openCameraFab = findViewById(R.id.homepage_fab_camera);
+        openCameraFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomePageActivity.this, StillOcrActivity.class));
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/example/mobilevision/StillOcrActivity.java
@@ -0,0 +1,141 @@
+package com.example.mobilevision;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.IOException;
+
+public class StillOcrActivity extends AppCompatActivity {
+
+    private String TAG = StillOcrActivity.class.getName();
+    private CameraSource cameraSource;
+    private SurfaceView surfaceCameraView;
+    private TextView priceDisplay;
+    private Button capture;
+    private TextRecognizer textRecognizer;
+    final static int REQUEST_CODE = 100;
+    boolean isPermissionGranted = false;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_still_ocr);
+
+        surfaceCameraView = findViewById(R.id.image_sv);
+//        priceDisplay = findViewById(R.id.price_tv);
+        capture = findViewById(R.id.capture_btn);
+        textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+
+        isPermissionGranted = requestCameraPermissions();
+
+        if(isPermissionGranted) {
+            if(textRecognizer.isOperational()) {
+                showCameraSource();
+            } else {
+                Log.d(TAG, "ERROR");
+            }
+        } else {
+            Toast.makeText(StillOcrActivity.this, "Permission Denied", Toast.LENGTH_SHORT).show();
+        }
+
+        capture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                captureImage();
+            }
+        });
+
+    }
+
+    private void captureImage() {
+        cameraSource.takePicture(null, new CameraSource.PictureCallback() {
+            @Override
+            public void onPictureTaken(byte[] bytes) {
+//                Frame billFrame = new Frame.Builder().setImageData(ByteBuffer.wrap(bytes), 400, 400, ImageFormat.NV21).build();
+                Bitmap billBitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                Frame billFrame = new Frame.Builder().setBitmap(billBitmap).build();
+                SparseArray<TextBlock> items = textRecognizer.detect(billFrame);
+                StringBuilder string = new StringBuilder();
+                for(int i = 0; i < items.size(); i++) {
+                    TextBlock item = items.valueAt(i);
+                    string.append(item.getValue());
+                    string.append("\n");
+                }
+                Toast.makeText(StillOcrActivity.this, string.toString(), Toast.LENGTH_LONG).show();
+//                priceDisplay.setText(string);
+            }
+        });
+    }
+
+    private void showCameraSource() {
+
+        cameraSource = new CameraSource.Builder(getApplicationContext(), textRecognizer)
+                .setAutoFocusEnabled(true)
+                .setFacing(CameraSource.CAMERA_FACING_BACK)
+                .setRequestedFps(2.0f)
+                .build();
+
+        surfaceCameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @SuppressLint("MissingPermission")
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    cameraSource.start(surfaceCameraView.getHolder());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+
+            }
+        });
+
+    }
+
+    private boolean requestCameraPermissions() {
+        if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(StillOcrActivity.this, new String[]{Manifest.permission.CAMERA}, REQUEST_CODE);
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQUEST_CODE && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            showCameraSource();
+        }
+    }
+}
