bcbf387ee647e1c3886422bddfa837490f15b231
Dhanasekhar <sekhardhana529@gmail.com>
2019-08-20 00:12:21 +0530

    Homepage design

---
+++ app/src/main/java/com/example/mobilevision/HomePageActivity.java
@@ -0,0 +1,15 @@
+package com.example.mobilevision;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+
+    }
+}
