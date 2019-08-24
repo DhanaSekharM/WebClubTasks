aa4b18c446bba2a023e1603ac9973523bf19aebd
Dhanasekhar <sekhardhana529@gmail.com>
2019-08-25 02:12:18 +0530

    Add http calls to dialogflow api

--- Chatbot/app/src/main/java/com/example/chatbot/ChatviewActivity.java
+++ Chatbot/app/src/main/java/com/example/chatbot/ChatviewActivity.java
@@ -7,16 +7,34 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.github.bassaer.chatmessageview.model.IChatUser;
 import com.github.bassaer.chatmessageview.model.Message;
 import com.github.bassaer.chatmessageview.util.ChatBot;
 import com.github.bassaer.chatmessageview.view.ChatView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ChatviewActivity extends AppCompatActivity {
 
     private ChatView chatView;
     private IChatUser user, bot;
+    private final static String TAG = ChatviewActivity.class.getName();
+    final String auth = "ya29.c.ElpuBzfCC-Eo24TCgPI8OJdmdn4bJsAPJ5tYkyYc0MYRTVfUgvlBx5DJN_jDMI5Nv6g_p740vby4iJVuyELGYjtqcXkYBCjK3eH_Gvb1lhkgRk_iDg7TvPOrLqE";
+    String url = "https://dialogflow.googleapis.com/v2/projects/chatbot-hetaqa/agent/sessions/123456789:detectIntent";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,7 +60,11 @@ public class ChatviewActivity extends AppCompatActivity {
                         .setText(chatView.getInputText())
                         .hideIcon(true)
                         .build();
-                makeRequest(chatView.getInputText());
+                try {
+                    makeRequest(chatView.getInputText());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
                 chatView.send(sendMessage);
                 chatView.setInputText("");
 
@@ -51,20 +73,81 @@ public class ChatviewActivity extends AppCompatActivity {
 
     }
 
-    private void makeRequest(String inputText) {
+    /**
+     * Make a post request to the dialogflow api to display the bot reply
+     *
+     * @param inputText user inputted text
+     */
+    private void makeRequest(String inputText) throws JSONException {
 
-        Message recMessage = new Message.Builder()
-                .setUser(bot)
-                .setRight(false)
-                .setText("Beep bop")
-                .hideIcon(true)
-                .build();
-        chatView.receive(recMessage);
+        JSONObject body = createRequestObject(inputText);
+
+        JsonObjectRequest request = new JsonObjectRequest
+                (Request.Method.POST, url, body,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                String reply = "";
+                                try {
+                                    reply = response.getJSONObject("queryResult").getString("fulfillmentText");
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                                Message recMessage = new Message.Builder()
+                                        .setUser(bot)
+                                        .setRight(false)
+                                        .setText(reply)
+                                        .hideIcon(true)
+                                        .build();
+                                chatView.receive(recMessage);
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(ChatviewActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+
+                            }
+                        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+
+                Map<String, String> header = new HashMap<>();
+                header.put("Content-Type", "application/json");
+                header.put("Authorization", "Bearer " + auth);
+
+                return header;
+            }
+        };
+        Volley.newRequestQueue(this)
+                .add(request)
+                .setTag(TAG);
+    }
+
+    /**
+     * creates the Json request body with the required format
+     *
+     * @param inputText
+     * @return final Json
+     */
+    private JSONObject createRequestObject(String inputText) throws JSONException {
+        Map<String, String> text = new HashMap<>();
+        JSONObject textObject = new JSONObject();
+        JSONObject requestObject = new JSONObject();
+
+        text.put("text", inputText);
+        text.put("language_code", "en-US");
+        JSONObject jsonObject = new JSONObject(text);
+        textObject.put("text", jsonObject);
+        requestObject.put("query_input", textObject);
+
+        return requestObject;
 
     }
 
     private void setChatViewAttributes() {
-        chatView.setRightBubbleColor(Color.RED);
+        chatView.setRightBubbleColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
         chatView.setLeftBubbleColor(Color.WHITE);
         chatView.setBackgroundColor(ContextCompat.getColor(this, R.color.lightBlue));
         chatView.setRightMessageTextColor(Color.BLACK);
@@ -96,5 +179,11 @@ public class ChatviewActivity extends AppCompatActivity {
 
             }
         };
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Volley.newRequestQueue(this).cancelAll(TAG);
     }
 }
